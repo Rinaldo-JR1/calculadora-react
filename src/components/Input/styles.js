c21774626664520e44ab21435c0caff71427ab0a
rinaldo-JR1 <rinaldo3uchoa@gmail.com>
2024-03-24 12:16:23 -0300

    End of project

---
+++ src/components/Input/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+    input{
+        width: 100%;
+        height: 75px;
+        padding: 0 10px;
+        
+        background-color: #AAAAFF;
+        color: #FFFFFF;
+        border: 0;
+    }
+`
